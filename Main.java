9dc660ed768ee2c7457585577c74cc74b5a719da
yzumite <101186651+ynukere@users.noreply.github.com>
2022-03-22 12:25:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    int parzysta=0;
+                for(int i=49;parzysta<=i;parzysta++)
+                  {
+                    if(parzysta%2==0)
+                      System.out.println(parzysta);
+                  }
+    System.out.println("Nieparzyste");
+        int nieparzysta=50;
+                for(int i=100;nieparzysta<=i;nieparzysta++)
+                  {
+                    if(nieparzysta%2==1)
+                      System.out.println(nieparzysta);
+                  }
+    
+  }
+}
